e4601636344347176e0ad3787b9c0331e18b1c77
ogwok <williamogwok10@gmail.com>
2023-06-19 13:46:30 +0300

    hide personal credentials

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 const { makeRequest } = require('./momoModule');
 
 makeRequest({
-  callbackHost: "https://webhook.site/318e088e-2f48-4ad4-8974-7615436d637a",
-  userApiKey: "f2d9072450404339b7ddc94b1d2692dd",
-  userId: "de4351ce-0bec-4073-ab06-f3b0d9ee4fb8",
-  primaryKey: "475706d56e944363b92d24829a13bb9c",
-  amount: "150",
-  currency: "EUR",
-  externalId: "txt_011",
-  partyIdType: "MSISDN",
-  partyId: "256784771634",
-  payerMessage: "testing",
-  payeeNote: "testing"
+  callbackHost: "<callbackHost>",
+  userApiKey: "<userApiKey>",
+  userId: "<userId>",
+  primaryKey: "<primaryKey>",
+  amount: "<amount>",
+  currency: "<currency>",
+  externalId: "<externalId>",
+  partyIdType: "<partyIdType..eg..MSISDN>",
+  partyId: "<partyId>",
+  payerMessage: "<payerMessage>",
+  payeeNote: "<payeeNote>"
 })
   .then(({ response, status }) => {
     console.log("Response:", response);
